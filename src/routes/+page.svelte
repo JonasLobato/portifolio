e0a869019dc8ea69f8fddb4a6b981be7073f0b3f
Jonas Lobato <jonaslobato50@gmail.com>
2024-10-29 20:10:32 -0300

    delete

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Sidebar from '../portifolio/components/Sidebar.svelte'; 
-</script>
-
-<Sidebar />
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
