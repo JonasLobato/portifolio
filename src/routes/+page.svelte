4023c727d3388caead75740ef6948b72d070eed4
Jonas Lobato <jonaslobato50@gmail.com>
2024-11-25 20:14:20 -0300

    recolve

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Hello Sveltekit JS</h1>
+<script>
+	import Header from "../lib/portifolio/header/Header.svelte";
+	import AboutMe from "../lib/portifolio/body/AboutMe.svelte";
+</script>
+
+<Header/>
+<AboutMe/>
