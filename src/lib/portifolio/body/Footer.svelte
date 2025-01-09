4857217d9173ce8f3b10f1b3f3d392394370d3d5
Jonas Lobato <jonaslobato50@gmail.com>
2025-01-09 11:41:37 -0300

    add Footer.svelte

---
+++ src/lib/portifolio/body/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="container">
+    <div>
+        <a target="_blank" href="https://github.com/JonasLobato">a</a>
+    </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import Header from "../lib/portifolio/header/Header.svelte";
 	import AboutMe from "../lib/portifolio/body/AboutMe.svelte";
 	import Works from "$lib/portifolio/body/Works.svelte";
+	import Footer from "../lib/portifolio/body/Footer.svelte";
 </script>
 
 <Header/>
 <Works/>
 <!-- <AboutMe/> -->
+ <Footer/>
